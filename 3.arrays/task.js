0f5e5a114c4dfdf64756abf845c459704d6924c3
Aleksandr Stupnikov <a-a-stupnikov@yandex-team.ru>
2024-12-03 16:01:25 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length){
+        return false
+    }
+ return arr1.every((element,index) => element === arr2[index])
+
 }
 
-function getUsersNamesInAgeRange(users, gender) {
   
+
+function getUsersNamesInAgeRange(users, gender) {
+
+    return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, arr) => acc + age / arr.length, 0);
 }
